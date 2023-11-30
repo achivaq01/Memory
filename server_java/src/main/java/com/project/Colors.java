cf8bcf387939ae62103e5835bf675050b71a1714
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2023-11-30 16:15:13 +0100

    implemented new methods

---
+++ server_java/src/main/java/com/project/Colors.java
@@ -0,0 +1,14 @@
+package com.project;
+
+public interface Colors {
+    public static final String WHITE = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String MAGENTA = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String BRIGHT_MAGENTA = "\u001B[95m";
+    public static final String BRIGHT_YELLOW = "\u001B[93m";  
+    
+} 
--- server_java/src/main/java/com/project/MemoryServer.java
+++ server_java/src/main/java/com/project/MemoryServer.java
@@ -3,25 +3,110 @@ package com.project;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.UUID;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+import org.json.JSONObject;
 
-class MemoryServer extends WebSocketServer {
-    private boolean isAlive;
+class MemoryServer extends WebSocketServer implements Colors {
+    // need to implement logType constants
+    private final HashMap<String, WebSocket> connectionMap;
+
     private static BufferedReader inputReader;
+    private boolean isAlive;
 
+    /**
+     * class constructor
+     * 
+     * @param port
+     */
     public MemoryServer(int port) {
-        super();
+        super(new InetSocketAddress(port));
 
         isAlive = false;
         inputReader = new BufferedReader(new InputStreamReader(System.in));
+        connectionMap = new HashMap<String, WebSocket>();
+    }
+
+    /**
+     * server bucle
+     * 
+     */
+    public void runServerBucle() {
+        isAlive = true;
+        String input = "";
+        
+        start();
+        while (isAlive) {
+            try {
+                input = inputReader.readLine();
+
+                if(input.equals("stop")) {
+                    isAlive = false;
+                }
+
+            } catch (IOException e)  {
+                serverLog(e, 0);
+            }
+        }
+
+        try {
+            stop(1000);
+        } catch (InterruptedException e) {
+            serverLog(e, 0);
+        }
+    }
+
+    /**
+     * server custom log method
+     * 
+     * @param exception
+     * @param logType
+     */
+    private void serverLog(Exception exception, int logType) {
+        String exceptionMessage = exception.getMessage();
+
+        switch (logType) {
+            default:
+                System.out.println(exceptionMessage);
+                break;
+
+        }
+    }
+
+    /**
+     * returns a random UUID
+     * 
+     * @return
+     */
+    public String getClientUUID() {
+        UUID clientUUID = UUID.randomUUID();
+
+        return clientUUID.toString();
+    }
+
+    /**
+     * sends a greetings message
+     * 
+     * @param connection
+     * @param id
+     */
+    private void sendGreetingsMessage(WebSocket connection, String id) {
+        JSONObject greetingsMessage = new JSONObject();
+
+        greetingsMessage.put("type", "greetings");
+        greetingsMessage.put("id", id);
     }
 
     @Override
-    public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        
+    public void onOpen(WebSocket connection, ClientHandshake handshake) {
+        String clientUUID = getClientUUID();
+        sendGreetingsMessage(connection, clientUUID);
+
     }
 
     @Override
@@ -41,33 +126,13 @@ class MemoryServer extends WebSocketServer {
 
     @Override
     public void onStart() {
-        
+        String host = getAddress().getAddress().getHostAddress();
+        int port = getAddress().getPort();
+        System.out.println("WebSockets server running at: ws://" + host + ":" + port);
+        System.out.println("Type 'stop' to stop and exit server.");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);        
+
     }
-
-    public void runServerBucle() {
-        isAlive = true;
-        String input = "";
-        
-        start();
-        while (isAlive) {
-            try {
-                input = inputReader.readLine();
-
-                if(input.equals("stop")) {
-                    isAlive = false;
-                }
-
-            } catch (IOException e)  {
-                // implement Log
-            }
-        }
-
-        try {
-            stop(1000);
-        } catch (InterruptedException e) {
-            // implement log
-        }
-    }
-
     
 }
